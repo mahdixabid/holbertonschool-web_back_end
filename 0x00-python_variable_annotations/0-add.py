8fcf064b0c11c446af11993500497d2176bf0f1f
mahdixabid <mahdiabid158@gmail.com>
2022-07-17 14:35:14 +0100

    update

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python3
-add = __import__('0-add').add
+"""
+    Module with typing Python
+"""
 
-print(add(1.11, 2.22) == 1.11 + 2.22)
-print(add.__annotations__)
+
+def add(a: float, b: float) -> float:
+    """
+        Args:
+            a: The first parameter
+            b: The second parameter
+
+        Return:
+            Sum of two floats
+
+    """
+
+    return (a + b)
